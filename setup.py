96f0722e511fa915f1921efbcae916d09da42e34
Karam Haj <36478430+Karamhaj1995@users.noreply.github.com>
2019-01-05 17:06:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,23 +6,6 @@ from shutil import copyfile
 from distutils.core import setup
 from distutils.dir_util import copy_tree
 
-
-# EK_CONF_FILE = "/etc/cityfix/conf/cityfix.conf"
-
-
-# Move Default Configuration for System Starter
-# if not isfile(EK_CONF_FILE):
-#     try:
-#         if not exists(dirname(EK_CONF_FILE)):
-#             makedirs(dirname(EK_CONF_FILE))
-#         copyfile("misc/Ekron-nms.conf-default", EK_CONF_FILE)
-#         copyfile("misc/db.conf", EK_DB_CONF_FILE)
-#         copyfile("misc/logging.conf", EK_LOG_CONF_FILE)
-
-#     except Exception as detail:
-#         print(detail)
-#         raise SystemExit
-
 setup(name='CityFix',
       version='0.1',
       description='CityFix Manager all city hazards and reports.',
